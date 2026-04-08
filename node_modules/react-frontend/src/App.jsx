02754ac43590ffc1d7fb426bf09fc3da1e3aac2d
Edgar Reynoso <reynosoedgar276@gmail.com>
2026-04-08 12:47:57 -0700

    Steps finished, needs debugging

--- node_modules/react-frontend/src/App.jsx
+++ node_modules/react-frontend/src/App.jsx
@@ -1,39 +1,27 @@
 import React from "react";
 import Table from "./Table";
 import { useState } from "react";
-
-
-const [characters, setCharacters] = useState([
-    {
-        name: "Charlie",
-        job: "Janitor"
-    },
-    {
-        name:"Mac",
-        job: "Bouncer"
-    },
-    {
-        name:"Dee",
-        job: "Aspiring actress"
-    },
-    {
-        name:"Dennis",
-        job: "Bartender"
-    }
-]);
-
-function removeOneCharacter(index) {
-  const updated = characters.filter((index, i) => {
-    return index !== i
-  });
-
-  setCharacters(updated);
-}
+import Form from "./Form";
 
 export default function MyApp(){
+  const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((index, i) => {
+      return index !== i
+    });
+
+    setCharacters(updated);
+  }
+
+  function updateList(person){
+    setCharacters([...characters], person);
+  }
+
   return (
     <div className="container">
-      <Table characterData = {characters} removeCharacter={removeOneCharacte}/>
+      <Table characterData = {characters} removeCharacter={removeOneCharacter}/>
+      <Form handleSubmit={updateList}/>
     </div>
   );
 }
---
+++ packages/react-frontend/src/Form.jsx
@@ -0,0 +1,42 @@
+import react, {useState} from "react";
+
+export default function Form(props){
+    const [person, setPerson] = useState({
+        name: "",
+        job: ""
+    });
+
+    function handleChange(event){
+        const { name, value } = event.target;
+        if (name === "job") setPerson({ name: person["name"], job: value});
+        else setPerson({name: value, job: person["job"]});
+    }
+
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({ name: "", job: "" });
+    }
+
+    return (
+        <Form>
+            <label htmlFor="name">Name</label>
+            <input
+                type= "text"
+                value= {person.name}
+                name="name"
+                id="name"
+                onChange={handleChange}
+            />
+            <label htmlFor="job"> Job</label>
+            <input
+                type="text"
+                value ={person.job}
+                name = "job"
+                id= "job"
+                onChange={handleChange}
+            />
+            <input type="button" value="Submit" onClick={submitForm} />
+        </Form> 
+    );
+}
+
